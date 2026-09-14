384d7f748fd2ba1ca284fb42da724c3e6ac06a9f
mattomar <moddathero@gmail.com>
2026-09-14 22:06:14 +0700

    made the friend button accepts inital status

--- src/components/friendButton.js
+++ src/components/friendButton.js
@@ -1,14 +1,18 @@
 import React, { useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
-import { sendFriendRequest } from "../utils/api";
+import {
+  sendFriendRequest,
+  acceptFriendRequest,
+  declineFriendRequest,
+} from "../utils/api";
 import { setFriendStatus } from "../store/friendSlice";
 
-const FriendButton = ({ userId }) => {
+const FriendButton = ({ userId, requestId, initialStatus }) => {
   const dispatch = useDispatch();
 
-  const status = useSelector(
-    (state) => state.friends.relations[userId] || "none",
-  );
+  const reduxStatus = useSelector((state) => state.friends.relations[userId]);
+
+  const status = reduxStatus || initialStatus || "none";
 
   const [loading, setLoading] = useState(false);
 
@@ -31,12 +35,47 @@ const FriendButton = ({ userId }) => {
     }
   };
 
+  const handleAccept = async () => {
+    try {
+      await acceptFriendRequest(requestId);
+
+      dispatch(
+        setFriendStatus({
+          userId,
+          status: "friends",
+        }),
+      );
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
+  const handleDecline = async () => {
+    try {
+      await declineFriendRequest(requestId);
+
+      dispatch(
+        setFriendStatus({
+          userId,
+          status: "none",
+        }),
+      );
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
   if (status === "pending_sent") {
     return <button disabled>Requested</button>;
   }
 
   if (status === "pending_received") {
-    return <button>Respond</button>;
+    return (
+      <>
+        <button onClick={handleAccept}>Accept</button>
+        <button onClick={handleDecline}>Decline</button>
+      </>
+    );
   }
 
   if (status === "friends") {
